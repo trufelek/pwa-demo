5c2f66abad2732ee790f7ca7429a90cb54d5ce8e
Aleksandra Wierzbiak <a.wierzbiak@wearevirtua.com>
2018-03-14 16:29:40 +0100

    Fixes

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 // doggy game
 var doggy = document.querySelector('.doggy');
 var bubble = document.querySelector('.bubble');
-var woofs = ['Wow', 'Such app', 'Much code', 'Woof woof', 'Hello there', 'Me hungry', 'Can I haz a cookie?', 'Gimme cookie pwease'];
+var woofs = ['Wow', 'Such app', 'Much code', 'Woof woof', 'Hello there', 'Me hungry', 'Can I haz a cookie?', 'Cookie pwease'];
 var barks = ['woof1', 'woof2'];
 
 // register service worker
