7a75e3a2a89c4741c62611dd9bca2363b9687f08
Aleksandra Wierzbiak <a.wierzbiak@wearevirtua.com>
2018-03-14 15:17:28 +0100

    PWA demo

---
+++ public/src/js/app.js
@@ -0,0 +1,33 @@
+// doggy game
+var doggy = document.querySelector('.doggy');
+var bubble = document.querySelector('.bubble');
+var woofs = ['Wow', 'Such app', 'Much code', 'Woof woof', 'Hello there', 'Me hungry', 'Can I haz a cookie?', 'Gimme cookie pwease'];
+var barks = ['woof1', 'woof2'];
+
+// register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function() {
+            console.log('SW registered');
+        });
+}
+
+doggy.addEventListener('click', function (event) {
+    var randomWoof = woofs[Math.floor((Math.random() * woofs.length))];
+    var randomBark = barks[Math.round(Math.random())];
+
+    bubble.classList.toggle('on');
+
+    if(bubble.classList.contains('on')) {
+        var bark = new Audio('src/sound/' + randomBark + '.wav');
+
+        bubble.innerHTML = randomWoof;
+        bark.play();
+
+        if(woofs.indexOf(randomWoof) > 4) {
+            doggy.classList.add('sits');
+        } else {
+            doggy.classList.remove('sits');
+        }
+    }
+});
---
+++ public/sw.js
@@ -0,0 +1,35 @@
+// service worker
+self.addEventListener('install', function (event) {
+    console.log('SW installed');
+    event.waitUntil(
+        caches.open('static')
+            .then(function (cache) {
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/css/app.css',
+                    '/src/img/dog-sit.png',
+                    '/src/img/dog-stand.png',
+                    'https://fonts.googleapis.com/css?family=Lobster'
+                ]);
+            })
+    );
+});
+
+self.addEventListener('activate', function () {
+    console.log('SW active');
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(res) {
+                if (res) {
+                    return res;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
+});
